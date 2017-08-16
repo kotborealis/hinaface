dd5bc9d89177ffb6d3069cb33fafef348057b752
kotborealis <kotborealis@gmail.com>
2017-08-16 14:04:59 +0300

    Async version

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const config = require('chen.js').config.resolve();
+const {promisify} = require('util');
 const http = require('http');
 const formidable = require('formidable');
 const fs = require('fs');
+const unlinkAsync = promisify(fs.unlink);
+const renameAsync = promisify(fs.rename);
+const readdirAsync = promisify(fs.readdir);
 const path = require('path');
 
 http.createServer((req, res) => {
@@ -9,17 +13,20 @@ http.createServer((req, res) => {
 	form.multiples = true;
 	form.hash = 'md5';
 
-	form.parse(req, (err, fields, files) => {
-		const _ = Object.keys(files).map(key => files[key]).map(file => {
+	form.parse(req, async (err, fields, files) => {
+		const _ = [];
+		for(let key in files){
+			const file = files[key];
+
 			if(file.size > config.upload.max_size){
-				fs.unlinkSync(file.path);
+				await unlinkAsync(file.path);
 				return null;
 			}
 
 			const name = file.hash + '.' + file.name.split('.').pop();
-			fs.renameSync(file.path, path.join(__dirname, config.upload.dir, name));
-			return name;
-		});
+			await renameAsync(file.path, path.join(__dirname, config.upload.dir, name));
+			_.push(name);
+		};
 
 		res.end(JSON.stringify(_.map(name => {
 			if(name === null) return name;
@@ -27,3 +34,5 @@ http.createServer((req, res) => {
 		})));
 	});
 }).listen(config.http.port);
+
+process.on('unhandledRejection', console.error.bind(console));
